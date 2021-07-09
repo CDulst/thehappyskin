d49fd17260fd218ddb2227ddf8265efd89e9803e
CDulst <casey.dulst@gmail.com>
2021-07-09 11:55:30 +0200

    buttons + begin styling header

---
+++ resources/views/buttons.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel = "stylesheet" href = "https://fonts.googleapis.com/css?family=Montserrat:200,400,700">
+    <link rel="stylesheet" href="css/app.css">
+    <script src="https://code.iconify.design/1/1.0.6/iconify.min.js"></script>
+    <title>thehappyskin</title>
+</head>
+<body>
+<a class = "button button--normal">
+<p class = "button--text">opleidingen</p>
+<span class="iconify" data-inline="false" data-icon="bi:arrow-right" style="margin-left:10px; font-size: 16px;"></span>
+</a>
+<a class = "button button--alt">
+<p class = "button--text">opleidingen</p>
+<span class="iconify" data-inline="false" data-icon="bi:arrow-right" style="margin-left:10px; font-size: 16px;"></span>
+</a>
+
+<a class = "button button--dark">
+<p class = "button--text">opleidingen</p>
+<span class="iconify" data-inline="false" data-icon="bi:arrow-right" style="margin-left:10px; font-size: 16px;"></span>
+</a>
+
+<a class = "button button--noborder">
+<p class = "button--text">opleidingen</p>
+<span class="iconify" data-inline="false" data-icon="bi:arrow-right" style="margin-left:10px; font-size: 16px;"></span>
+</a>
+<script src = "js/app.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,10 +7,34 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel = "stylesheet" href = "https://fonts.googleapis.com/css?family=Montserrat:200,400,700">
     <link rel="stylesheet" href="css/app.css">
+    <script src="https://code.iconify.design/1/1.0.6/iconify.min.js"></script>
     <title>thehappyskin</title>
 </head>
 <body>
-<h1>Hello<h1>
+<header class = "header">
+<div class="container-fluid header--top">
+</div>
+<div class = "container">
+<div class="row header--bottom">
+<div class = "col-3 logo--container">
+<img class = "logo--small"src="/images/logo-small.svg" alt="logo">
+</div>
+<div class = "col-9">
+<nav class = "navigator">
+<ul class = "menu__items">
+<li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Merken</a></li>
+<li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Skin health attitude</a></li>
+<li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Producten</a></li>
+<li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Verkooppunten</a></li>
+<li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Over ons</a></li>
+<li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Professional</a></li>
+<li class = "menu__item special__item"><a href="javascript:void(0)" class = "menu__item--link">Contact me</a></li>
+</ul>
+</nav>
+</div>
+</div>
+</div>
+<header>
 <script src = "js/app.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
