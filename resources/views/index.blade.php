be2e5cd2f4b25d25b83b188c444e81487613a68d
CDulst <casey.dulst@gmail.com>
2021-07-09 13:30:53 +0200

    title section + background video

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,63 +1,74 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel = "stylesheet" href = "https://fonts.googleapis.com/css?family=Montserrat:200,400,700">
-    <link rel="stylesheet" href="css/app.css">
-    <script src="https://code.iconify.design/1/1.0.6/iconify.min.js"></script>
-    <title>thehappyskin</title>
-</head>
-<body>
-<header class = "header">
-<div class="container-fluid header--top">
-<div class = "container h-100">
-<div class = "row justify-content-between h-100">
-<div class = "col-3 h-100">
-<a class = "button button--header">
-<p class = "button--text">Maak een afspraak voor je huidanalyse</p>
-<span class="iconify" data-inline="false" data-icon="bi:arrow-right" style="margin-left:10px; font-size: 16px;"></span>
-</a>
-</div>
-<div class = "col-6 h-100">
-<nav class = "navigator">
-<ul class = "menu__items">
-<li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Magazine</a></li>
-<li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Events</a></li>
-<li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Contact</a></li>
-<li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Login</a></li>
-<li class = "menu__item"><form><input class = "search__input" type="text" placeholder = "Search" href="javascript:void(0)" class = "menu__item--link"></form></li>
-<li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">NL</a></li>
-</ul>
-</nav>
-</div>
-</div>
-</div>
-</div>
-<div class = "container">
-<div class="row justify-content-between header--bottom">
-<div class = "col-3 logo--container">
-<img class = "logo--small"src="/images/logo-small.svg" alt="logo">
-</div>
-<div class = "col-9">
-<nav class = "navigator">
-<ul class = "menu__items">
-<li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Merken</a></li>
-<li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Skin health attitude</a></li>
-<li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Producten</a></li>
-<li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Verkooppunten</a></li>
-<li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Over ons</a></li>
-<li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Professional</a></li>
-<li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Contact me</a></li>
-</ul>
-</nav>
-</div>
-</div>
-</div>
-<header>
-<script src = "js/app.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel = "stylesheet" href = "https://fonts.googleapis.com/css?family=Montserrat:200,400,700">
+        <link rel="stylesheet" href="css/app.css">
+        <script src="https://code.iconify.design/1/1.0.6/iconify.min.js"></script>
+        <title>thehappyskin</title>
+    </head>
+    <body>
+        <header class = "header">
+            <div class="container-fluid header--top">
+                <div class = "container h-100">
+                    <div class = "row justify-content-between h-100">
+                        <div class = "col-3 h-100">
+                            <a class = "button button--header">
+                                <p class = "button--text">Maak een afspraak voor je huidanalyse</p>
+                                <span class="iconify" data-inline="false" data-icon="bi:arrow-right" style="margin-left:10px; font-size: 16px;"></span>
+                            </a>
+                        </div>
+                        <div class = "col-6 h-100">
+                            <nav class = "navigator">
+                                <ul class = "menu__items">
+                                    <li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Magazine</a></li>
+                                    <li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Events</a></li>
+                                    <li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Contact</a></li>
+                                    <li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Login</a></li>
+                                    <li class = "menu__item">
+                                        <form><input class = "search--input" type="text" placeholder = "Search" href="javascript:void(0)" class = "menu__item--link"></form>
+                                    </li>
+                                    <li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">NL</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class = "container">
+                <div class="row justify-content-between header--bottom">
+                    <div class = "col-3 logo--container">
+                        <img class = "logo--small invert"src="/images/logo-small.svg" alt="logo">
+                    </div>
+                    <div class = "col-9">
+                        <nav class = "navigator">
+                            <ul class = "menu__items">
+                                <li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Merken</a></li>
+                                <li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Skin health attitude</a></li>
+                                <li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Producten</a></li>
+                                <li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Verkooppunten</a></li>
+                                <li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Over ons</a></li>
+                                <li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Professional</a></li>
+                                <li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Contact me</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <main>
+            <video class = "backgroundvideo" autoplay muted loop>
+                <source src="videos/background.mp4" type="video/mp4">
+                Your browser does not support the video tag.
+            </video>
+            <section class = "section titlesection centerH centerV  flex">
+                <h1 class = "white">skin health attitude</h1>
+            </section>
+        </main>
+        <script src = "js/app.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </body>
 </html>
