6756cc8444c9596943c02c379b6e79f29a7a9d0b
CDulst <casey.dulst@gmail.com>
2021-07-09 12:59:31 +0200

    basic header finished

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,9 +13,31 @@
 <body>
 <header class = "header">
 <div class="container-fluid header--top">
+<div class = "container h-100">
+<div class = "row justify-content-between h-100">
+<div class = "col-3 h-100">
+<a class = "button button--header">
+<p class = "button--text">Maak een afspraak voor je huidanalyse</p>
+<span class="iconify" data-inline="false" data-icon="bi:arrow-right" style="margin-left:10px; font-size: 16px;"></span>
+</a>
+</div>
+<div class = "col-6 h-100">
+<nav class = "navigator">
+<ul class = "menu__items">
+<li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Magazine</a></li>
+<li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Events</a></li>
+<li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Contact</a></li>
+<li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Login</a></li>
+<li class = "menu__item"><form><input class = "search__input" type="text" placeholder = "Search" href="javascript:void(0)" class = "menu__item--link"></form></li>
+<li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">NL</a></li>
+</ul>
+</nav>
+</div>
+</div>
+</div>
 </div>
 <div class = "container">
-<div class="row header--bottom">
+<div class="row justify-content-between header--bottom">
 <div class = "col-3 logo--container">
 <img class = "logo--small"src="/images/logo-small.svg" alt="logo">
 </div>
@@ -28,7 +50,7 @@
 <li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Verkooppunten</a></li>
 <li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Over ons</a></li>
 <li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Professional</a></li>
-<li class = "menu__item special__item"><a href="javascript:void(0)" class = "menu__item--link">Contact me</a></li>
+<li class = "menu__item"><a href="javascript:void(0)" class = "menu__item--link">Contact me</a></li>
 </ul>
 </nav>
 </div>
